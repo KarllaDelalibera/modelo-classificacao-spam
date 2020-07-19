d3c30eb547545440f434cade2c6461132acb6b90
Karlla Delalibera <karlladelalibera@gmail.com>
2020-07-19 16:35:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pathlib import Path
 import pickle
 import streamlit as st
 
-parent_path = parent_path = Path(__file__).parents[0]
+parent_path = Path(__file__).parents[0]
 
 @st.cache
 def carrega_dados(path):
@@ -20,7 +21,7 @@ def carrega_modelo_treinado(path):
 
 def predict_modelo(modelo, texto):
     predicao = modelo.predict([texto])[0]
-    predicao_prob = np.max(modelo.predict_proba([texto]))
+    predicao_prob = np.round(np.max(modelo.predict_proba([texto])),2)
     return [predicao, predicao_prob]
 
 
@@ -28,21 +29,33 @@ def main():
     df = carrega_dados(path=parent_path / 'dataset/dados.csv')
     modelo = carrega_modelo_treinado(path=parent_path / 'model/spam-nb-model.pkl')
 
-    page = st.sidebar.selectbox("Opções:", ["Objetivo", "Visualização dos dados", "Predição"])
+    st.sidebar.title("Menu de opções")
+    page = st.sidebar.selectbox("Selecione uma página:", ["Objetivo", "Visualização dos dados", "Predição"])
 
     if page == 'Objetivo':
-        st.title("Modelo de classificação de Spam")
+        st.title("Modelo de detecção de Spam")
+        st.markdown("### :email: Objetivo")
+        st.markdown("Essa é uma aplicação utilizando Streamlit e Scikit-learn para prevermos se um e-mail é spam ou não.")
 
+        st.markdown("### :email: Informações sobre a modelagem")
+        st.markdown("* Dados: Os dados desssa modelagem foram extraídos do Kaggle [link](https://www.kaggle.com/team-ai/spam-text-message-classification).")
+        st.markdown("* Modelagem: O modelo considerado foi um Naive Bayes.")
+    
     elif page == "Visualização dos dados":
         st.title("Conhecendo os dados")
-        st.header("Dados de estudo")
 
+        st.markdown("### :game_die: Dados de estudo")
         st.dataframe(df, width=900, height=300)
-        st.text('Fonte: https://www.kaggle.com/team-ai/spam-text-message-classification')
 
+        st.markdown("### :game_die: Distribuição das classes")
+        classes = df.groupby(['Category']).count() 
+        classes.plot.bar()
+        st.pyplot()
+        st.markdown("De 5572 mensagens, temos 4825 e-mail's classificados como normais (ham) e 747 classificados como spam.")
+    
     elif page == "Predição":
         st.title("Prevendo se um e-mail é spam ou não")
-        user_input = st.text_input('Insira um trecho do e-mail', '')
+        user_input = title = st.text_input('Insira um trecho do e-mail (em inglês)', '')
         resul_predicao = predict_modelo(modelo, user_input)
         st.write('O e-mail é: {pred}, com probabilidade {prob}'.format(pred=resul_predicao[0],
                                                                        prob=resul_predicao[1]))
@@ -50,5 +63,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# streamlit run app.py
